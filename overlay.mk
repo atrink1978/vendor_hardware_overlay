50c5e2251d1c30270d5bd949fc57c8eb195396e2
Ubuntu <ubuntu@xenial-aosp.lxd>
2020-11-01 19:05:25 +0100

    add all devices

---
+++ overlay.mk
@@ -0,0 +1,24 @@
+PRODUCT_PACKAGES += \
+	HardwareOverlayPicker \
+	QtiAudio \
+	TrebleApp \
+	treble-overlay-NavBar \
+	treble-overlay-NightMode \
+	treble-overlay-SystemUI-FalseLocks \
+	treble-overlay-Telephony-LTE \
+	treble-overlay-caf-ims \
+	treble-overlay-devinputjack \
+	treble-overlay-mtk-ims \
+	treble-overlay-webview \
+	treble-overlay-wifi5g \
+
+
+PRODUCT_PACKAGES += \
+	hw-fpnav-daemon \
+
+#PRODUCT_COPY_FILES += \
+#	vendor/hardware_overlay/Huawei/Fingerprint/hw-fingerprint.rc:system/etc/init/hw-fingerprint.rc \
+#	vendor/hardware_overlay/Huawei/Fingerprint/hw-fpnav:system/bin/hw-fpnav \
+#	vendor/hardware_overlay/Huawei/Fingerprint/hw-fpnav.dex:system/phh/hw-fpnav.dex \
+#	vendor/hardware_overlay/Huawei/Fingerprint/fingerprint.kl:system/phh/huawei/fingerprint.kl \
+
